96e4078ec65c3c9804e006864e5a8b8ded87a4b0
Akanksha-07-01-98 <akanksha070198@gmail.com>
2026-05-02 10:12:37 +0530

    added models and IDataService

---
+++ StockDashboardApp/SimpleTraderDomain/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        public User AccountHolder {  get; set; }
+
+        public double Balance {  get; set; }
+
+        public IEnumerable<AssetTransaction> AssetTransactions{  get; set; }
+
+        public IEnumerable<Asset> Assets { get; set; }
+    }
+}
---
+++ StockDashboardApp/SimpleTraderDomain/Asset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+
+        public Account Account {  get; set; }
+
+        public Stock Stock { get; set; }
+
+        public int Shares {  get; set; }
+    }
+}
